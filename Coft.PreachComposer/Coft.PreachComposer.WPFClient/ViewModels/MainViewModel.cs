98e92f91f2053ad7741e4a54650db89c32714a6e
radoslaw.nagrabski@gmail.com <radoslaw.nagrabski@gmail.com>
2016-07-14 22:18:07 +0200

    init

---
+++ Coft.PreachComposer/Coft.PreachComposer.WPFClient/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using GalaSoft.MvvmLight.Messaging;
+using System.IO.Compression;
+using System.IO;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace Coft.PreachComposer.WPFClient.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private RelayCommand processCommand;
+
+        public RelayCommand ProcessCommand
+        {
+            get { return processCommand; }
+        }
+
+
+        public MainViewModel()
+        {
+
+        }
+
+    }
+}
---
+++ Coft.PreachComposer/Coft.PreachComposer.WPFClient/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,33 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coft.PreachComposer.WPFClient.ViewModels
+{
+    public class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            if (ViewModelBase.IsInDesignModeStatic)
+            {
+            }
+
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel Main
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+    }
+}
